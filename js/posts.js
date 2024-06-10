f3b3a2e1acc73d68a8b2796b7e0d1ea143072323
tukainpng <dfern3321@gmail.com>
2024-06-10 19:56:22 -0300

    Small modification

--- js/posts.js
+++ js/posts.js
@@ -1,9 +1,9 @@
 let headings = document.querySelectorAll("h1, h2, h3")
+
 document.write('<div class="headings"><ul>')
-for ( let i = 0; i < headings.length; i++) {
-  headings[i].id = headings[i].innerHTML
-  document.write('<li><a href="#' + headings[i].innerHTML + '">')
-  document.write(headings[i].innerHTML)
-  document.write('</a></li>')
-}
+  for (let i = 0; i < headings.length; i++) {
+    headings[i].id = headings[i].innerHTML
+    document.write('<li><a href="#' + headings[i].innerHTML + '">')
+    document.write(headings[i].innerHTML + '</a></li>')
+  }
 document.write("<ul></div>")
